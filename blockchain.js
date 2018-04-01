47287d00040d204b83f8f0b6fdeb042fa6fd0002
Tung Dao <me@tungdao.com>
2018-04-01 23:50:38 +0700

    initial commit

---
+++ blockchain.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto')
+
+
+class Block {
+  constructor(index, previousHash, timestamp, data, nonce) {
+    this.index = index
+    this.previousHash = previousHash
+    this.timestamp = timestamp
+    this.nonce = nonce
+    this.hash = null
+  }
+}
+
+
+class Node {
+  constructor() {
+
+  }
+
+  start(genesis=null) {
+    console.log('Node started.')
+
+    if (!genesis) {
+      // TODO: broadcast ourself, waiting for data from peer
+    }
+  }
+}
+
+
+exports.Block = Block
+exports.Node = Node
---
+++ index.js
@@ -0,0 +1,4 @@
+const blockchain = require('./blockchain')
+
+const node = new blockchain.Node()
+node.start()
